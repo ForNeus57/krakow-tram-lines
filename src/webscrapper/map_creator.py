e55579374199e3d1da593926a0f3cf10f403b650
ForNeus57 <dominikbreksa@gmail.com>
2023-10-21 00:45:48 +0200

    Added tram train tracks data gathering and little visualization to .PNG format

--- src/webscrapper/map_creator.py
+++ src/webscrapper/map_creator.py
@@ -1,5 +1,13 @@
+import osmnx as ox
+
+from constraints import LOCATION
+
+
 def gather_data():
-    pass
+    g = ox.project_graph(ox.graph_from_place(LOCATION, custom_filter=r'["railway"~"tram"]'))
+
+    ox.plot_graph(g, show=True, filepath="data/generated/tram_tracks.png", figsize=(64, 64), node_size=0,
+                  edge_linewidth=2, edge_color="#ffffff", save=True)
 
 
 def main():
