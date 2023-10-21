b2e34fa396ff301416c9e7d71c588a09452c6d8c
ForNeus57 <dominikbreksa@gmail.com>
2023-10-21 20:17:31 +0200

    Preprocessed first vehicles data batch

--- src/acquisition/osmnx/constraints.py
+++ src/acquisition/osmnx/constraints.py
@@ -2,6 +2,3 @@ CITY = 'Krakow'
 VOIVODESHIP = 'Lesser Poland'
 COUNTRY = 'Poland'
 LOCATION = f'{CITY}, {VOIVODESHIP}, {COUNTRY}'
-
-URL_TRAM_MODELS = 'https://api.ttss.pl/vehicles/trams/'
-
---
+++ src/acquisition/osmnx/tram_tracks.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import osmnx as ox
+from networkx import MultiDiGraph
+from geopandas import GeoDataFrame
+
+from src.acquisition.osmnx.constraints import LOCATION
+
+
+class TramTracksData:
+    """
+    Class that ..... # TODO docstring
+    """
+
+
+def get_train_track_graph() -> MultiDiGraph:
+    return ox.graph_from_place(LOCATION, custom_filter=r'["railway"~"tram"]')
+
+
+def get_tram_stops() -> GeoDataFrame:
+    return ox.features_from_place(LOCATION, tags={'railway': 'tram_stop'})
