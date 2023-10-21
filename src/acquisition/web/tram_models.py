17dcde2e5186b88dcf63b725bf693a4b28c1d3b9
ForNeus57 <dominikbreksa@gmail.com>
2023-10-21 23:38:32 +0200

    Preprocessed first vehicles data batch

--- src/acquisition/web/tram_models.py
+++ src/acquisition/web/tram_models.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+
+import re
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -36,22 +38,23 @@ class TramModelsData:
         vehicles_in_ttss: pd.DataFrame = pd.read_pickle(f"{path}/vehicles_by_ttss.pkl")
         return cls(vehicles_by_line, vehicles_by_type, vehicles_in_ttss, path)
 
+    @staticmethod
+    def split_tram_names(x):
+        items = str(x).split(" ")
+        new_items = []
+        for tram_id in items:
+            if len(tram_id) == TRAM_NAME_LENGTH and tram_id[0].isalpha():
+                new_items.append(tram_id[0: TRAM_NAME_LENGTH])
+
+        return new_items
+
     @classmethod
     def preprocess_vehicles_by_line(cls, vehicles_by_line: pd.DataFrame) -> pd.DataFrame:
-        def split_tram_names(x):
-            items = str(x).split(" ")
-            new_items = []
-            for tram_id in items:
-                if len(tram_id) == TRAM_NAME_LENGTH and tram_id[0].isalpha():
-                    new_items.append(tram_id[0: TRAM_NAME_LENGTH])
-
-            return new_items
-
-        tram_id_column_name = "tram_id"
-        line_number_column_name = "line_number"
+        tram_id_column_name = "id"
+        line_number_column_name = "line"
         new_data = pd.DataFrame(data=None, columns=[tram_id_column_name, line_number_column_name])
 
-        for idx, row in vehicles_by_line.apply(split_tram_names).items():
+        for idx, row in vehicles_by_line.apply(TramModelsData.split_tram_names).items():
             if not idx.isnumeric():
                 continue
             for item in row:
@@ -61,8 +64,46 @@ class TramModelsData:
 
     @classmethod
     def preprocess_vehicles_by_type(cls, vehicles_by_type: pd.DataFrame) -> pd.DataFrame:
-        return vehicles_by_type
+        """
+        #TODO: change this to delete row 3....
+        :param vehicles_by_type:
+        :return:
+        """
+        vehicles_by_type.drop(1, inplace=True, axis=1)
+        vehicles_by_type[2] = vehicles_by_type[2].apply(TramModelsData.split_tram_names)
+        vehicles_by_type.set_index(vehicles_by_type[0], inplace=True)
+        vehicles_by_type[0] = vehicles_by_type[2]
+        vehicles_by_type.drop(2, inplace=True, axis=1)
+        tram_id_column_name = "id"
+        train_name_column_name = "name"
+        new_data = pd.DataFrame(data=None, columns=[tram_id_column_name, train_name_column_name])
+        for idx, row in vehicles_by_type[0].items():
+            for item in row:
+                new_data.loc[len(new_data)] = {tram_id_column_name: item, train_name_column_name: idx}
+        return new_data
 
     @classmethod
     def preprocess_vehicles_by_ttss(cls, vehicles_by_ttss: pd.DataFrame) -> pd.DataFrame:
+        missing_data = '?'
+
+        def change_column_names(x: str) -> str:
+            value = re.sub(r'[^\w\s-]', '', x.lower())
+            return re.sub(r'[-\s]+', '-', value).strip('-_')
+
+        vehicles_by_ttss.columns = [change_column_names(col) for col in vehicles_by_ttss.columns]
+        # TODO: merge into one
+        vehicles_by_ttss.drop(vehicles_by_ttss[vehicles_by_ttss["line"].str.contains(r'\?') |
+                                               vehicles_by_ttss["vehicle"].str.contains(r'\?')].index,
+                              inplace=True, axis=0)
+        vehicles_by_ttss['position'] = vehicles_by_ttss['position'].apply(lambda x: str(x).split(","))
+        # TODO: Check if this precision is sufficient
+        vehicles_by_ttss['latitude'] = vehicles_by_ttss['position'].apply(lambda x: x[0]).astype(float)
+        vehicles_by_ttss['longitude'] = vehicles_by_ttss['position'].apply(lambda x: x[1]).astype(float)
+
+        vehicles_by_ttss['line'] = vehicles_by_ttss['line'].apply(lambda x: str(x).split(" "))
+        vehicles_by_ttss['line_name'] = vehicles_by_ttss['line'].apply(lambda x: ' '.join(x[1:]))
+        vehicles_by_ttss['line'] = vehicles_by_ttss['line'].apply(lambda x: x[0]).astype(int)
+        vehicles_by_ttss.drop('position', axis=1, inplace=True)
+        print(vehicles_by_ttss.to_string())
+        print(len(vehicles_by_ttss))
         return vehicles_by_ttss
