121b10a595702a0860611713f97e985b6bad9786
Miłosz Góralczyk <goralczyk.milosz2k@gmail.com>
2023-11-28 23:47:45 +0100

    added people generator to model, very temp

--- src/ktl/aggregation/__main__.py
+++ src/ktl/aggregation/__main__.py
@@ -1,19 +1,15 @@
 from ktl.aggregation.create_json_lines import save_json_to_file
-<<<<<<< HEAD
 from ktl.aggregation.aggregate_tracks import aggregate_tracks
-
-
-def main() -> None:
-    #save_json_to_file()
-    aggregate_tracks()
-=======
 from ktl.aggregation.merge.tram_data_mergere import merge_xslx_data_to_from_ttss
 
-
 def main() -> None:
-    # save_json_to_file()
+
+
+
+    save_json_to_file()
+    aggregate_tracks()
     merge_xslx_data_to_from_ttss()
->>>>>>> f45d0aca1f7b9bb41a75aeeb01e42622ac2e7020
+
 
 
 if __name__ == "__main__":
