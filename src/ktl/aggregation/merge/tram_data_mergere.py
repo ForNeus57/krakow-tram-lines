21d33c7a3206f0f16f8eb3fd8871153bf342df73
ForNeus57 <dominikbreksa@gmail.com>
2023-12-01 10:52:46 +0100

    Zawoajanie done

--- src/ktl/aggregation/merge/tram_data_mergere.py
+++ src/ktl/aggregation/merge/tram_data_mergere.py
@@ -1,5 +1,7 @@
 from typing import Tuple, List
 
+import re
+
 import pandas as pd
 import json
 
@@ -11,6 +13,92 @@ def merge_xslx_data_to_from_ttss() -> None:
     departures = pd.read_pickle("./data/generated/pickle/departures.pkl")
     time_table = pd.read_pickle("./data/generated/pickle/time_table.pkl")
 
+    result = prepare_tram_trains_data(vehicles_type, attributes, lines)
+
+    out: dict[str, List[Tuple[str, int, int]]] = {}
+    start_time: int = 7
+    end_time: int = 19
+
+    def helper(x):
+        return re.sub(r' \d{2}$', '', x)
+
+    departures = departures.where(start_time <= departures['hour']).dropna()
+    departures = departures.where(departures['hour'] <= end_time).dropna()
+    departures['absolute'] = departures.apply(lambda x: x['hour'] * 60 + x['minute'], axis=1).astype(int)
+    departures['name'] = departures['name'].apply(lambda x: re.sub(r' \d{2}$', '', x))
+
+    for _, row in result.iterrows():
+        identifier: str = str(row['id'])
+        line: str = str(row['line'])
+        out[identifier] = []
+
+        stops: pd.DataFrame = time_table[(time_table['line'] == line)]
+        if len(stops) == 0:
+            continue
+        stops = stops.sort_values(by=['order'], ascending=True)
+        stops['name'] = stops['name'].apply(lambda x: re.sub(r' \d{2}$', '', x))
+
+        first_stop: str = str(stops.iloc[0]['name'])
+
+        mode: str = 'START_END'
+        reversed_mode: str = 'END_START'
+
+        prev_absolute_time: int = \
+            departures[(departures['name'] == first_stop) & (departures['line'] == line)].iloc[0]['absolute']
+
+        while True:
+            found = False
+            for _, stop in stops.iloc[:-1].iterrows():
+                stop_name: str = stop['name']
+                # print(stop_name)
+                suitable_hours: pd.DataFrame = (departures[(departures['name'] == stop_name) &
+                                                           (departures['line'] == line) &
+                                                           (departures['direction'] == mode) &
+                                                           (departures['absolute'] > prev_absolute_time)]
+                                                .sort_values(by=['hour', 'minute'], ascending=True))
+                if not len(suitable_hours) > 0:
+                    found = False
+                    break
+
+                found = True
+                prev_absolute_time = suitable_hours.iloc[0]['absolute']
+                departures.drop(index=suitable_hours.index[0], axis=0, inplace=True)
+                out[identifier].append((stop_name, suitable_hours.iloc[0]['hour'], suitable_hours.iloc[0]['minute']))
+
+            if not found:
+                break
+
+            # print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+
+            for _, stop in stops.sort_values(by=['order'], ascending=False).iloc[:-1, :].iterrows():
+                stop_name: str = stop['name']
+                # print(stop_name)
+                suitable_hours: pd.DataFrame = (departures[(departures['name'] == stop_name) &
+                                                           (departures['line'] == line) &
+                                                           (departures['direction'] == reversed_mode) &
+                                                           (departures['absolute'] > prev_absolute_time)]
+                                                .sort_values(by=['hour', 'minute'], ascending=True))
+                if not len(suitable_hours) > 0:
+                    found = False
+                    break
+
+                found = True
+                prev_absolute_time = suitable_hours.iloc[0]['absolute']
+                departures.drop(index=suitable_hours.index[0], axis=0, inplace=True)
+                out[identifier].append((stop_name, suitable_hours.iloc[0]['hour'], suitable_hours.iloc[0]['minute']))
+                # print(identifier, out[identifier])
+
+            # print('BBBBBBBBBBBBBBBBBBBBBBBB')
+
+            if not found:
+                break
+
+    save_data(result, out)
+
+
+def prepare_tram_trains_data(vehicles_type: pd.DataFrame,
+                             attributes: pd.DataFrame,
+                             lines: pd.DataFrame) -> pd.DataFrame:
     vehicles_type['merge'] = vehicles_type.apply(
         lambda x: str(x['tram_depo_code']) + str(x['tram_code']) + str(x['id']), axis=1)
 
@@ -19,59 +107,15 @@ def merge_xslx_data_to_from_ttss() -> None:
     result = pd.merge(vehicles_type, attributes, how="inner", on="name")
     result = pd.merge(result, lines, how="inner", on="id")
     result.drop(['last_seen', 'latitude', 'longitude', 'direction'], axis=1, inplace=True)
-    result.to_json("./data/generated/json/vehicles_full_data.json", orient="records", force_ascii=False)
+    return result
 
-    out: dict[str, List[Tuple[str, int, int]]] = {}
-    start_time: int = 7
-    end_time: int = 19
 
-    departures = departures.where(start_time <= departures['hour']).dropna()
-    departures = departures.where(departures['hour'] <= end_time).dropna()
-    departures['absolute'] = departures.apply(lambda x: x['hour'] * 60 + x['minute'], axis=1)
-
-    for idx, row in result.iterrows():
-        id: str = str(row['id'])
-        line: str = str(row['line'])
-        out[id] = []
-        stops: pd.DataFrame = time_table.where(time_table['line'] == line).dropna()
-        if stops.size == 0:
-            continue
-        stops.sort_values(by=['order'], inplace=True, ascending=True)
-        first_stop: str = str(stops.iloc[0]['name'])
-        prev_absolute_time: float = \
-        departures.where(departures['name'] == first_stop).dropna().where(departures['line'] == line).dropna().iloc[0][
-            'absolute']
-
-        for _, stop in stops.iterrows():
-            stop_name: str = str(stop['name'])
-            suitable_hours = departures.where(departures['name'] == stop_name).dropna()
-            suitable_hours = suitable_hours.where(suitable_hours['line'] == line).dropna()
-            suitable_hours = suitable_hours.where(suitable_hours['direction'] == 'START_END').dropna()
-            suitable_hours = suitable_hours.where(suitable_hours['absolute'] > prev_absolute_time).dropna()
-            suitable_hours.sort_values(by=['hour', 'minute'], inplace=True)
-
-            if suitable_hours.size == 0:
-                continue
-            idx = suitable_hours.index.values[0]
-            prev_absolute_time = suitable_hours.iloc[0]['absolute']
-            out[id].append((stop_name, suitable_hours.iloc[0]['hour'], suitable_hours.iloc[0]['minute']))
-            departures.drop([idx], inplace=True)
-
-        stops.sort_values(by=['order'], inplace=True, ascending=False)
-
-        for _, stop in stops.iterrows():
-            stop_name: str = str(stop['name'])
-            suitable_hours = departures.where(departures['name'] == stop_name).dropna()
-            suitable_hours = suitable_hours.where(suitable_hours['line'] == line).dropna()
-            suitable_hours = suitable_hours.where(suitable_hours['direction'] == 'END_START').dropna()
-            suitable_hours = suitable_hours.where(suitable_hours['absolute'] > prev_absolute_time).dropna()
-            suitable_hours.sort_values(by=['absolute'], inplace=True)
-            if suitable_hours.size == 0:
-                continue
-            idx = suitable_hours.index.values[0]
-            prev_absolute_time = suitable_hours.iloc[0]['absolute']
-            out[id].append((stop_name, suitable_hours.iloc[0]['hour'], suitable_hours.iloc[0]['minute']))
-            departures.drop([idx], inplace=True)
+def save_data(trains_data: pd.DataFrame, trains_schedule_data: dict[str, List[Tuple[str, int, int]]]) -> None:
+    trains_data.to_json("./data/generated/json/vehicles_full_data.json", orient="records", force_ascii=False)
 
     with open("./data/generated/json/vehicles_arrival_times.json", "w", encoding="utf-8") as output_file:
-        json.dump(out, output_file, indent=4, ensure_ascii=False)
+        json.dump(trains_schedule_data, output_file, indent=4, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    merge_xslx_data_to_from_ttss()
